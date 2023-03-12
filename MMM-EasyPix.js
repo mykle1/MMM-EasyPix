398d7de4c619ddd78eeb847a1fa9417528b3ef33
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-03-12 06:36:56 +0100

    Handle linter issues and rework

--- MMM-EasyPix.js
+++ MMM-EasyPix.js
@@ -1,92 +1,81 @@
-/* Magic Mirror
+/* global Module */
+/* MagicMirror²
  * Module: MMM-EasyPix
  *
  * By Mykle1
  * MIT Licensed.
  */
 Module.register("MMM-EasyPix", {
+  defaults: {
+    picName: "face.gif", // .jpg, .gif, .png, etc. (animated gif's too!)
+    maxWidth: "100%", // your picture files go in "pix" folder of module
+    sounds: ["1.mp3", "me2.mp3"], // mp3 audio file names in quotes seperated by commas
+    updateInterval: 30 * 60 * 1000, //  Updates display (Milliseconds) - Default: 30 * 60 * 1000 = 30 minutes
+    animationSpeed: 3000 // Speed of the update animation. (Milliseconds)
+  },
 
-    defaults: {
-        picName: "face.gif",          // .jpg, .gif, .png, etc.. (animated gif's too!)
-		maxWidth: "100%",             // your picture files go in "pix" folder of module
-		sounds: ["1.mp3", "me2.mp3"],  // mp3 sound file names in quotes seperated by commas
-		updateInterval: 30 * 60 * 1000,     // updates display
-		animationSpeed: 3000,
-		},
+  start() {
+    const self = this;
+    this.url = "";
 
-    start: function() {
-        var self = this;
-        this.url = '';
+    // ADDED: Schedule update timer courtesy of ninjabreadman
+    setInterval(() => {
+      self.updateDom(self.config.animationSpeed || 0); // use config.animationSpeed or revert to zero @ninjabreadman
+    }, this.config.updateInterval);
+  },
 
-        // ADDED: Schedule update timer courtesy of ninjabreadman
-       var self = this;
-       setInterval(function() {
-       self.updateDom(self.config.animationSpeed || 0); // use config.animationSpeed or revert to zero @ninjabreadman
-       }, this.config.updateInterval);
-    },
+  getStyles() {
+    return ["MMM-EasyPix.css"];
+  },
 
+  // Override dom generator.
+  getDom() {
+    const wrapper = document.createElement("div");
+    const image = document.createElement("img");
+    image.src = `/modules/MMM-EasyPix/pix/${
+      this.config.picName
+    }?seed=${new Date()}`;
+    image.className = "mmm-easypix-photo";
+    image.style.maxWidth = this.config.maxWidth;
+    wrapper.appendChild(image);
+    return wrapper;
+  },
 
+  random_imglink() {
+    const myimages = "/modules/MMM-EasyPix/pix/";
 
-    getStyles: function() {
-        return ["MMM-EasyPix.css"]
-    },
+    let ry = Math.floor(Math.random() * "/modules/MMM-EasyPix/pix/".length);
+    if (ry === 0) {
+      ry = 1;
+    }
+    document.write(`<img src="${myimages[ry]}" border=0>`);
+  },
 
+  /// //  Add this function to the modules you want to control with voice //////
+  /// //  Must be the same as in "sentences" array in MMM-voice.js /////
+  /// //  Replace audio file with your own greeting /////
 
+  notificationReceived(notification) {
+    if (notification === "HIDE_LUCY") {
+      this.hide(500);
+    } else if (notification === "SHOW_LUCY") {
+      this.show(1000);
+    }
 
-
-    // Override dom generator.
-    getDom: function() {
-        var wrapper = document.createElement("div");
-        var image = document.createElement("img");
-            image.src = '/modules/MMM-EasyPix/pix/' + this.config.picName + "?seed=" + new Date();
-            image.className = "photo";
-			image.style.maxWidth = this.config.maxWidth;
-        wrapper.appendChild(image);
-        return wrapper;
-    },
-
-    random_imglink: function (){
-    var myimages = new Array()
-    var myimages = '/modules/MMM-EasyPix/pix/';
-
-    var ry = Math.floor(Math.random()*'/modules/MMM-EasyPix/pix/'.length)
-  if (ry==0) {
-     ry=1;
-}
-    document.write('<img src="'+myimages[ry]+'" border=0>');
-
-    return random_imglink;
-},
-
-
-
-/////  Add this function to the modules you want to control with voice //////
-/////  Must be the same as in "sentences" array in MMM-voice.js /////
-/////  Replace sound file with your own greeting /////
-
-    notificationReceived: function(notification, payload) {
-        if (notification === 'HIDE_LUCY') {
-            this.hide(500);
-        }  else if (notification === 'SHOW_LUCY') {
-            this.show(1000);
-        }
-
-        if (notification === 'HELLO_THERE_LUCY') {
-            var sound = new Audio();
-            sound.src = 'modules/MMM-EasyPix/hello.mp3';
-            sound.play();
-        }
- ///////// So you don't hear the same greeting every time /////////////////////////
-////////// Randomized sound files courtesy of @ Cowboysdude ////////////////////////
-	   if (notification === 'SHOW_LUCY') {
-            var audio_files = this.config.sounds;
-            var random_file = audio_files[Math.floor(Math.random() * audio_files.length)];
-            var audio = new Audio(random_file);
-            audio.src = 'modules/MMM-EasyPix/sounds/'+random_file;
-            audio.play();
-        }
-
-    },
-
-
+    if (notification === "HELLO_THERE_LUCY") {
+      const sound = new Audio();
+      sound.src = "modules/MMM-EasyPix/hello.mp3";
+      sound.play();
+    }
+    /// ////// So you don't hear the same greeting every time /////////////////////////
+    /// /////// Randomized sound files courtesy of @ Cowboysdude ////////////////////////
+    if (notification === "SHOW_LUCY") {
+      const audioFiles = this.config.sounds;
+      const randomFile =
+        audioFiles[Math.floor(Math.random() * audioFiles.length)];
+      const audio = new Audio(randomFile);
+      audio.src = `modules/MMM-EasyPix/sounds/${randomFile}`;
+      audio.play();
+    }
+  }
 });
--- README.md
+++ README.md
@@ -1,39 +1,44 @@
-## MMM-EasyPix
+# MMM-EasyPix
 
-The easiest way to show pictures onto your mirror.
+The easiest way to show pictures on your MagicMirror².
 
-.jpg files, .png files, .gif files, etc..  (Even animated gif's work)
+Supported file formats: jpg, png and gif (even animated gif's).
 
 ## Where your picture files go
 
-* Your picture files go in the "pix" folder inside the MMM-EasyPix module folder.
+Your picture files go in the "pix" folder inside the MMM-EasyPix module folder.
 
-## Works with Hello-Lucy voice enhancement (required for sound)
+## Works with Hello-Lucy
+
+This module works also with [Hello-Lucy](https://github.com/mykle1/Hello-Lucy) voice enhancement (required for sound).
+
+Pi users should use static pictures or small sized gifs. They can try `eyes.gif` or `2.gif`.
 
 ## Installation
 
-* `git clone https://github.com/mykle1/MMM-EasyPix` into the `~/MagicMirror/modules` directory.
+- `git clone https://github.com/mykle1/MMM-EasyPix` into the `~/MagicMirror/modules` directory.
+- Annotated .css file included for exact positioning.
 
-* Annotated .css file included for exact positioning.
+## config.js entry and options
 
-## Config.js entry and options
+```js
+  {
+    module: "MMM-EasyPix",
+    position: "top_center",
+    config: {
+      picName: "18.jpg",              // Enter the picture file name.
+      maxWidth: "75%",                // Size picture precisely. Retains aspect ratio.
+      sounds: ["1.mp3", "me2.mp3"],   // mp3 audio file names in quotes seperated by commas for Hello-Lucy
+      updateInterval: 30 * 60 * 1000, // Updates display (Milliseconds) - Default: 30 * 60 * 1000 = 30 minutes
+      animationSpeed: 3000,           // Speed of the update animation. (Milliseconds)
+      }
+    },
+```
 
-    {
-        module: "MMM-EasyPix",
-		position: "top_center",
-		config: {
-			picName: "18.jpg", // Enter the picture file name.
-			maxWidth: "75%",        // Size picture precisely. Retains aspect ratio.
-			sounds: ["1.mp3", "me2.mp3"],  // mp3 sound file names in quotes seperated by commas for Hello-Lucy
-			updateInterval: 30 * 60 * 1000,     // updates display
-		        animationSpeed: 3000,
-		}
-	},
-	
-## How to use it multiple times!
+## How to use it multiple times
 
-* Simply make multiple entries in your config and name a different picture.
+- Simply make multiple entries in your config and name a different picture.
 
 ## Remember
 
-* All picture files go in the "pix" folder inside the MMM-EasyPix module folder.
+- All picture files go in the "pix" folder inside the MMM-EasyPix module folder.
